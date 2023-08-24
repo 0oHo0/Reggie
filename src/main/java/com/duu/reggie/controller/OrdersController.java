d2bee85ed22c26d10997378432bcb459137d30d4
duu <472287500@qq.com>
2023-08-24 22:21:06 +0800

    暂存

--- src/main/java/com/duu/reggie/controller/OrdersController.java
+++ src/main/java/com/duu/reggie/controller/OrdersController.java
@@ -1,6 +1,7 @@
 package com.duu.reggie.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.duu.reggie.common.R;
 import com.duu.reggie.entity.OrderDetail;
@@ -13,8 +14,10 @@ import org.slf4j.Logger;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -62,5 +65,83 @@ public class OrdersController {
         ).collect(Collectors.toList());
         ordersDtoPage.setRecords(list);
         return R.success(ordersDtoPage);
+    }
+
+    @Transactional
+    @PostMapping("/again")
+    public R<String> again(@RequestBody Orders order){
+        Long orderId = order.getId();
+        Orders orders = ordersService.getById(orderId);
+        Long newOrderId = IdWorker.getId();
+        orders.setId(newOrderId);
+        String number = newOrderId.toString();
+        orders.setNumber(number);
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setCheckoutTime(LocalDateTime.now());
+        orders.setStatus(2);
+        ordersService.save(orders);
+        LambdaQueryWrapper<OrderDetail> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(OrderDetail::getOrderId,orderId);
+        List<OrderDetail> list = orderDetailService.list(queryWrapper);
+        list.stream().map((item)->{
+            long detailId = IdWorker.getId();
+            item.setOrderId(newOrderId);
+            item.setId(detailId);
+            return item;
+        }).collect(Collectors.toList());
+        orderDetailService.saveBatch(list);
+        return R.success("再来一单");
+    }
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String number,String beginTime,String endTime){
+        //构造分页构造器
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+
+        Page<OrdersDto> ordersDtoPage=new Page<>();
+        //构造条件构造器
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        //根据number进行模糊查询
+        queryWrapper.like(!StringUtils.isEmpty(number),Orders::getNumber,number);
+        //根据Datetime进行时间范围查询
+
+//        log.info("开始时间：{}",beginTime);
+//        log.info("结束时间：{}",endTime);
+        if(beginTime!=null&&endTime!=null){
+            queryWrapper.ge(Orders::getOrderTime,beginTime);
+            queryWrapper.le(Orders::getOrderTime,endTime);
+        }
+        //添加排序条件
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+
+        //进行分页查询
+        ordersService.page(pageInfo,queryWrapper);
+
+        //对象拷贝
+        BeanUtils.copyProperties(pageInfo,ordersDtoPage,"records");
+
+        List<Orders> records=pageInfo.getRecords();
+
+        List<OrdersDto> list=records.stream().map((item)->{
+            OrdersDto ordersDto=new OrdersDto();
+
+            BeanUtils.copyProperties(item,ordersDto);
+            String name="用户"+item.getUserId();
+            ordersDto.setUserName(name);
+            return ordersDto;
+        }).collect(Collectors.toList());
+
+        ordersDtoPage.setRecords(list);
+        return R.success(ordersDtoPage);
+    }
+    @PutMapping
+    public R<String> send(@RequestBody Orders orders){
+        Long id = orders.getId();
+        Integer status = orders.getStatus();
+        LambdaQueryWrapper<Orders> queryWrapper=new LambdaQueryWrapper<>();
+        queryWrapper.eq(Orders::getId,id);
+        Orders one = ordersService.getOne(queryWrapper);
+        one.setStatus(status);
+        ordersService.updateById(one);
+        return R.success("派送成功");
     }
 }
